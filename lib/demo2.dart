f5ced198a1f8c9a624d3578071ea363e199f3c74
rahul <rahulrekm@gmail.com>
2022-07-12 13:30:22 +0530

    commit

---
+++ lib/demo2.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main(){
+
+  int age = 30;
+
+  var name = "rahul";
+
+  var emailId = "rahulrekm@gmail.com";
+
+  final course = "Andrpoid Flutter";
+
+  stdout.write("My name is $name" + " \n my age is $age \n"
+               + "my email-id is $emailId"
+               + " \n I am studying $course" + " in luminarlabs"
+  );
+
+
+}
