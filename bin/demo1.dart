edd6856b3bb6e4f0a4366ee3346967b6f3244af9
rahul <rahulrekm@gmail.com>
2022-07-12 12:40:59 +0530

    commit

---
+++ bin/demo1.dart
@@ -0,0 +1,15 @@
+import 'package:demo1/demo1.dart' as demo1;
+
+
+
+void main(){
+  String name = "Rahul";
+  int age = 36;
+  String email = "rahulrekm@gmail.com";
+  double cgpa = 67.5;
+
+  print("My name is ${name}");
+  print("I am i $age years old");
+  print("My email id is $email ");
+  print(" I secured $cgpa% marks in MCA");
+}
