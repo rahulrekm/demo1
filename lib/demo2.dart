eab9b553d7b9f4ed32d9d1461463a366dc56a341
rahul <rahulrekm@gmail.com>
2022-07-13 12:52:47 +0530

    commit

--- lib/demo2.dart
+++ lib/demo2.dart
@@ -6,16 +6,21 @@ void main(){
 
   var name = "rahul";
 
-  var add = 20;
+ // var add = 20;
 
   var emailId = "rahulrekm@gmail.com";
 
-  final course = "Andrpoid Flutter";
+  final course = "Android Flutter";
 
   stdout.write("My name is $name" + " \n my age is $age \n"
                + "my email-id is $emailId"
                + " \n I am studying $course" + " in luminarlabs"
   );
 
-
+  name+= " R";
+  stdout.write("\n");
+  stdout.write("$name");
+  age += 40;
+  stdout.write("\n");
+  print("$age");
 }
---
+++ lib/exampleclassdemo.dart
@@ -0,0 +1,30 @@
+class Student_details{
+  // instance variables depends on object  object.variablename
+ late String name;
+ late int age;
+ late String email;
+ late int mobile;
+ // static variables depends on class    classname.variablename
+ static final String course =  "Flutter";
+
+}
+
+   void main(){
+
+   // object creation  => classname objectname = classname(); constructor
+    //                   => var objectname     = classname();
+
+     Student_details student1 = Student_details();
+
+     print("Student 1 name = ${student1.name = "Rahul"}");
+     print("Student 1 age = ${student1.age = 36}");
+     print("Student 1 email = ${student1.email = "rahulrekm@gmail.com"}");
+     print("Student 1 mobile = ${student1.mobile = 9895670679}");
+
+     print("Student 1 course = ${Student_details.course}");
+
+
+
+
+
+}
