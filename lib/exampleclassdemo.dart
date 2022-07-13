371dbfc0c637396f4aedd1b70a6e36c092d3cd53
rahul <rahulrekm@gmail.com>
2022-07-13 13:21:44 +0530

    second

--- lib/exampleclassdemo.dart
+++ lib/exampleclassdemo.dart
@@ -7,6 +7,17 @@ class Student_details{
  // static variables depends on class    classname.variablename
  static final String course =  "Flutter";
 
+ // user defined default function
+    void institute(){
+      print("Institute is Luminar Technolabs");
+    }
+
+  // user defined parametrized function
+     void hobby(String myhobby){  // where myhobby is parameter/argument/formal parameter
+      print("My hobby is $myhobby");
+     }
+
+
 }
 
    void main(){
@@ -20,9 +31,11 @@ class Student_details{
      print("Student 1 age = ${student1.age = 36}");
      print("Student 1 email = ${student1.email = "rahulrekm@gmail.com"}");
      print("Student 1 mobile = ${student1.mobile = 9895670679}");
-
      print("Student 1 course = ${Student_details.course}");
      print("\n");
+     student1.hobby("Reading");
+     student1.institute();
+
 
      Student_details student2 = Student_details();
 
@@ -30,9 +43,11 @@ class Student_details{
      print("Student 2 age = ${student2.age = 26}");
      print("Student 2 email = ${student2.email = "arun@gmail.com"}");
      print("Student 2 mobile = ${student2.mobile = 9895670000}");
-
      print("Student 2 course = ${Student_details.course}");
      print("\n");
+     student2.institute();
+     student2.hobby("Chess");
+
 
 
 
