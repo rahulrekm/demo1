c42b8c6358e3d3a84a9c98984eb9ee55663ad4d2
rahul <rahulrekm@gmail.com>
2022-07-14 11:26:05 +0530

    mobile

--- lib/exampleclassdemo2.dart
+++ lib/exampleclassdemo2.dart
@@ -42,7 +42,7 @@ class Mobiles{
   print("Manufacturing year is ${mobile_1.manf_year = 2022 }");
   print("Screen size is ${mobile_1.screen_size = 6.1}inches");
   print("Ram capacity of this mobile os ${mobile_1.ram = 4}GB");
-  print("Processor speed is ${mobile_1.processor_speed = 3.23}Ghz");
+  print("Processor speed is ${mobile_1.processor_speed = 3.23}GHz");
   print("Processor Type is ${mobile_1.processor_type = "Hexa-core"}");
   print("Battery Capacity is ${mobile_1.battery_capacity = 3095}mAh");
   print("Primary Camera of this model is ${mobile_1.primary_camera = 36}MP");
@@ -50,5 +50,41 @@ class Mobiles{
   
   mobile_1.network_type("4G");
   mobile_1.storage_capacity("1 TB");
-  
+
+
+  Mobiles mobile_2 = Mobiles();
+
+  print("Mobile Type is $Mobiles.mobile_type");
+  print("Brand name is ${mobile_2.brand_name = "Samsung "} ");
+  print("Model Number of the mobile is ${mobile_2.model_no = "Galaxy F12" }");
+  print("Manufacturing year is ${mobile_2.manf_year = 2022 }");
+  print("Screen size is ${mobile_2.screen_size = 6.4}inches");
+  print("Ram capacity of this mobile os ${mobile_2.ram = 6}GB");
+  print("Processor speed is ${mobile_2.processor_speed = 2.0}GHz");
+  print("Processor Type is ${mobile_2.processor_type = "Octa-core"}");
+  print("Battery Capacity is ${mobile_2.battery_capacity = 6000}mAh");
+  print("Primary Camera of this model is ${mobile_2.primary_camera = 56}MP");
+  print("Secondary Camera is ${mobile_2.secondary_camera = 13}MP");
+
+  mobile_1.network_type("4G");
+  mobile_1.storage_capacity("12 GB");
+
+
+  Mobiles mobile_3 = Mobiles();
+
+  print("Mobile Type is $Mobiles.mobile_type_2");
+  print("Brand name is ${mobile_3.brand_name = " Nokia "} ");
+  print("Model Number of the mobile is ${mobile_3.model_no = " 105 SS " }");
+  print("Manufacturing year is ${mobile_3.manf_year = 2022 }");
+  print("Screen size is ${mobile_3.screen_size = 1.77}inches");
+  print("Ram capacity of this mobile os ${mobile_3.ram = 32}MB");
+  print("Processor speed is ${mobile_3.processor_speed = 0.208}GHz");
+  print("Processor Type is ${mobile_3.processor_type = "SC6531E"}");
+  print("Battery Capacity is ${mobile_3.battery_capacity = 800}mAh");
+  print("Primary Camera of this model is ${mobile_3.primary_camera = 0}MP");
+  print("Secondary Camera is ${mobile_3.secondary_camera = 0}MP");
+
+  mobile_1.network_type("2G");
+  mobile_1.storage_capacity("32 MB");
+
   }
